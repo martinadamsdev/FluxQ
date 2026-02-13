870be2df6ed7e9bd17c67a6b5e8551c7c30e53da
martinadams.dev <martinadams.dev@gmail.com>
2026-02-13 20:29:09 +0800

    Initial Commit

---
+++ FluxQTests/FluxQTests.swift
@@ -0,0 +1,16 @@
+//
+//  FluxQTests.swift
+//  FluxQTests
+//
+//  Created by martinadamsdev on 2026/2/13.
+//
+
+import Testing
+
+struct FluxQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
